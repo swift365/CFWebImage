e57624a762f78580b1574d7dff98610606a59c85
衡成飞 <hchengfei@dafeifei.local>
2016-11-22 15:22:39 +0800

    add image

--- Example/CFWebImage/ViewController.swift
+++ Example/CFWebImage/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import CFWebImage
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+
+        let placeImage = UIImage(named: "1")
+        
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        
+        self.view.addSubview(imageView)
+        
+        imageView.cf_setImage(url: NSURL(string: "http://i1.s1.dpfile.com/pc/2ade09cd3b74b377810f2bff4a0c0964%28700x700%29/thumb.jpg")!, placeHolderImage: placeImage)
     }
 
 }
